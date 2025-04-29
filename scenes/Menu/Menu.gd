4664a58d64326919b706d29d7a2a19154aaae263
Tomás <tomasmendez@outlook.com>
2025-04-29 11:09:30 -0300

    1

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -9,3 +9,11 @@ func _ready():
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+
+
+func _on_red_generator_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_violet_generator_button_down() -> void:
+	pass # Replace with function body.
